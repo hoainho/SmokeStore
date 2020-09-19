8dc544765ccb5294233630c500ccf87907046446
RemAlw <nhoxtvt@gmail.com>
2020-09-19 16:34:55 +0700

    css for logo

--- khoistore/src/App.js
+++ khoistore/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+// import { BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div className="container">
+        <div className="logo"> 
+          <span className="logo-mix logo-mix_1 ">K</span>
+          <span className="logo-mix logo-mix_2 ">H</span>
+          <span className="logo-mix logo-mix_3 ">O</span>
+          <span className="logo-mix logo-mix_4 ">I</span>
+        </div>
+      </div>
     </div>
   );
 }
