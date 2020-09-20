94f416e92c6ac559692cbde595bf7b7de847e2a7
RemAlw <nhoxtvt@gmail.com>
2020-09-20 23:16:26 +0700

    Add navbar basic

---
+++ khoistore/src/components/home/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function navbar() {
+  return (
+        <div className="navbar">
+                <ul className="navbar-list">
+                  <li className="navbar-item">
+                    <a href className="navbar-link">HOME</a>
+                  </li>
+                  <li className="navbar-item">
+                    <a href className="navbar-link">SHOP</a>
+                  </li>
+                  <li className="navbar-item navbar-item--logo">
+                        <span className="navbar-item--logo_mix navbar-item--logo_mix__1">K</span>
+                        <span className="navbar-item--logo_mix navbar-item--logo_mix__2">H</span>
+                        <span className="navbar-item--logo_mix navbar-item--logo_mix__3">O</span>
+                        <span className="navbar-item--logo_mix navbar-item--logo_mix__4">I</span>
+                    
+                  </li>
+                  <li className="navbar-item">
+                    <a href className="navbar-link">COLLECTIONS</a>
+                  </li>
+                  <li className="navbar-item">
+                    <a href className="navbar-link">ABOUT</a>
+                  </li>
+                </ul>
+        </div>
+  );
+}
