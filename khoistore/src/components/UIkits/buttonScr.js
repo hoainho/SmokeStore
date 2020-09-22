e3017adb0faeeda235848b612fcf0bac19eefe2e
RemAlw <nhoxtvt@gmail.com>
2020-09-22 23:03:34 +0700

    button css

---
+++ khoistore/src/components/UIkits/buttonScr.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function App() {
+  return (
+       <div className="buttonScr">
+            <button className="buttonScr-item">
+            &#129043;
+            </button>
+       </div>
+  );
+}
