2961703825104a5a5bd727fc8eef670f756ed929
RemAlw <nhoxtvt@gmail.com>
2020-09-23 15:54:04 +0700

    css full screen home

---
+++ khoistore/src/components/home/Arrival/ArrItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function ArrivalItem() {
+  return (
+       <div className="Arrival-item">
+            <img src="../../image/bgFashion.jpg" alt="iconFashion" />
+       </div>
+  );
+}
--- khoistore/src/components/home/index.js
+++ khoistore/src/components/home/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from './navbar';
 import Smoke from '../UIkits/smoke';
+import Button from '../UIkits/buttonScr';
+import Arrival from './Arrival/Arrival';
 export default function App() {
   return (
     <div>
        <div className="home">
-        <Navbar/>
-        <Smoke/>
+          <Navbar/>
+          <Smoke/>
+          <Button/>
        </div>
+       <Arrival/>
     </div>
   );
 }
