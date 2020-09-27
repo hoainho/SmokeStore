0f0194639b9670c3084ccbb3d86fac8abd22f83d
RemAlw <nhoxtvt@gmail.com>
2020-09-27 21:00:08 +0700

    sad

--- khoistore/src/components/home/Arrival/Arrival.js
+++ khoistore/src/components/home/Arrival/Arrival.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ArrivalItem from './ArrItem';
 export default function Arrival() {
   return (
     <div>
        <div className="Arrival">
             <div className="Arrival-container">
-                <ArrivalItem/>
-                <ArrivalItem/>
             </div>
        </div>
     </div>
