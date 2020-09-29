25d923e08fe87f52e5d0c74ba85e9e1208662be4
RemAlw <nhoxtvt@gmail.com>
2020-09-29 23:49:41 +0700

    push css arrival :

---
+++ khoistore/src/components/home/Arrival/Arrival.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export default function Arrival() {
+  return (
+    <div>
+       <div className="Arrival">
+            <div className="Arrival-container">
+              
+            </div>
+       </div>
+    </div>
+  );
+}
